1dcf2297756725abd9ae905dafcd293e08049b34
DalyssX <82532054+DalyssX@users.noreply.github.com>
2021-05-14 13:54:14 +0300

    ver1

---
+++ BikeLib.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Lab__02ALib
+{
+    class Bike
+    {
+        public string Brand;
+        public string Country;
+        public string Region;
+        public int Price;
+        public double Weight;
+        public double Lenghth;
+        public bool HasHydraulicBrake;
+        public bool HasHydraulicSeatPost;
+        public double GetYearIncomePerInhabitant()
+        {
+            return Price / Weight;
+        }
+
+    }
+}
